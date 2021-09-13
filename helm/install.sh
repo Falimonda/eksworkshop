c86ab8260c5216c5dd56a53274baa782f8d92272
Falimonda <filippo.alimonda@gmail.com>
2021-09-13 00:18:05 +0000

    Organizes files

---
+++ helm/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+helm version --short
+
+helm repo add stable https://charts.helm.sh/stable
+
+helm search repo stable
+
+helm completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+source <(helm completion bash)
--- initialization/setup.sh
+++ initialization/setup.sh
@@ -21,8 +21,8 @@ kubectl completion bash >>  ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 
-echo 'export LBC_VERSION="v2.2.0"' >>  ~/.bash_profile
-.  ~/.bash_profile
+echo 'export LBC_VERSION="v2.2.0"' >>  ~/.bashrc
+.  ~/.bashrc
 
 
 
